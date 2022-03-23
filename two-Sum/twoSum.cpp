b43ff9f6f2253556e52d4f14f3fd8d057a1ef4de
STohfa <fsamihatohfa@gmail.com>
2022-03-23 11:48:53 -0600

    Completed twoSum problem

--- two-Sum/twoSum.cpp
+++ two-Sum/twoSum.cpp
@@ -1,10 +1,17 @@
-#include<iostream>
+#include<bits/stdc++.h>
 
-using namespace std;
-
-int main()
-{
-    cout<< "hellow world";
-
-   return 0; 
-}
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> umap;
+        int idx=0;
+        for(auto el:nums)
+            umap[el]=idx++;
+        for(int i=0;i<nums.size();i++){
+            if(umap.find(target-nums[i])!=umap.end() and i!=umap[target-nums[i]])
+                return {i, umap[target-nums[i]]};
+        }
+        
+        return {};
+    }
+};
